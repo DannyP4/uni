36d740b140405515a11313d936a9a17ea075bff4
Da Liem <119425892+DannyP4@users.noreply.github.com>
2024-08-22 22:53:32 +0700

    Delete $RECYCLE.BIN/S-1-5-21-2665600683-3389849866-3735459040-1001 directory

--- $RECYCLE.BIN/S-1-5-21-2665600683-3389849866-3735459040-1001/$RO43YC2/20232/IT3280/week4/bai3/bai3b.asm
+++
@@ -1,3 +0,0 @@
-.text 
-	li	$s1, 10
-	add	$s0, $s1, $zero		# $s0 = $s1
--- $RECYCLE.BIN/S-1-5-21-2665600683-3389849866-3735459040-1001/$RO43YC2/20232/IT3280/week4/bai3/bai3d.asm
+++
@@ -1,9 +0,0 @@
-.text
-	li	$s1, 3
-	li	$s2, 5
-	sle 	$t0, $s1, $s2	# $t0 = $s1 <= $s2 ? 1 : 0 
-	bnez 	$t0, label
-	j	exit
-label:
-	li	$s3, 10		# neu $s1 <= $s2 thi $s3 = 10
-exit:
